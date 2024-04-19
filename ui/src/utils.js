d411c3a0c16b07547c4b1269670928c8d211f9d0
loren <lorenmichaelh@gmail.com>
2024-04-19 13:08:33 -0500

    fixed connection issue, updating API URL and bringing tasks map back in

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://i7tedyjptoyl2nqye32vaboegq0zoddw.lambda-url.us-east-2.on.aws/task"
+export const API_URL = "https://z2cixqscwew722uvizrmmlxghe0ukbcl.lambda-url.us-east-2.on.aws/task"
