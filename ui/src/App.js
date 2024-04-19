54d2f5abf3a66d08d246fd294ebf9b44893b9649
loren <lorenmichaelh@gmail.com>
2024-04-19 12:00:33 -0500

    comment out task map to get UI working on host

--- ui/src/App.js
+++ ui/src/App.js
@@ -33,9 +33,9 @@ function App() {
     <ThemeProvider theme={ darkTheme }>
       <CssBaseline />
       <AddTaskForm fetchTasks={fetchTasks} />
-      {tasks.map((task) => (
+      {/* {tasks.map((task) => (
         <Task key={task.id} task={task} fetchTasks={fetchTasks} />
-      ))}
+      ))} */}
     </ThemeProvider>
   );
 };
